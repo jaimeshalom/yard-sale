9eea5a49996a391ff7853e31aa019294c0d7b6b3
jaimeshalom <jaimeshalom12@gmail.com>
2021-11-20 11:16:21 -0400

    Fix homepage en package

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    filename: 'yard-sale/bundle.js',
+    publicPath: '/yard-sale/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
